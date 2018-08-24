c5ef0b6446d65650bf9b5d4c9c20d1bd16f07800
veselinn16 <veselinn16@gmail.com>
2018-08-24 14:59:20 +0300

    Remove functionality comment :pencil:

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-/*
-GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the switch player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the switch player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
 let scores, roundScore, currentPlayer, gamePlaying;
 
 const diceDOM = document.querySelector('.dice');
